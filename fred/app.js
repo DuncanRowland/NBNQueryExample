2d99791e6a6ffd0b5ad8b1a91037328b348d611d
Peter Neish <peter.neish@gmail.com>
2013-01-30 21:50:45 +1100

    adding in json data

--- fred/app.js
+++ fred/app.js
@@ -2,45 +2,34 @@
 // testing simple example
 
 
-var dataset = [];                        //Initialize empty array
-for (var i = 0; i < 5; i++) {           //Loop 25 times
-    var newNumber = Math.random() * 30;  //New random number (0-30)
-    dataset.push(newNumber);             //Add new number to array
-}
 
 //Width and height
 var w = 500;
-var h = 500;
-				
-var svg = d3.select("body").append("svg");
-svg.attr("width", w)
-   .attr("height", h);
-   
-var circles = svg.selectAll("circle")
-                 .data(dataset)
-                 .enter()
-                 .append("circle");
-				 
-circles.attr("cx", function(d, i) {
-            return (i * 50) + 25;
-        })
-       .attr("cy", h/2)
-       .attr("r", function(d) {
-            return d;
-       })
-	   .attr("fill", "yellow")
-.attr("stroke", "orange")
-.attr("stroke-width", function(d) {
-    return d/2;
-});			
+var h = 100;
 
-var circle = svg.selectAll("circle")
-    .data([32, 57, 150], String);
+var data;
 
-circle.enter().append("circle")
-    .attr("cy", 90)
-    .attr("cx", String)
-    .attr("r", Math.sqrt);
+jQuery.getJSON("http://biodiversity.org.au/apni.name/28354.json?callback=?", function(error, json){
+  //if (error) return console.warn(error);
+  data = json;
+  alert(data[0]);
+  visualizeit();
+	console.log("hello");
 
-circle.exit().remove();	 
-   
+	
+});
+
+function visualizeit(){
+  				
+	var svg = d3.select("body").append("svg");
+	svg.attr("width", w)
+	   .attr("height", h);
+
+    svg.data(data[0]).selectAll("div")
+	.enter().append("div")
+	.text(function(d){ return "Helllllo";});	   
+	   
+}
+
+
+
